16c561c86ccf30e62b0e546adb48b982ea5fff82
Hatim Jamali <hatimjamali@Hatims-MacBook-Air.local>
2025-06-04 18:58:42 +0500

    Your update

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-      
+if __name__ == "__main__":
+    app.run()
+# This file is used to run the application.
+# It imports the create_app function from the app module and runs the application.
+# The application will run with the default settings defined in the app module.
+# You can run this file to start the application server.   
